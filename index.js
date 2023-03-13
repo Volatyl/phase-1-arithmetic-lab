78cdc36a7dbbb52fe76c8602edc87e5a1c188714
Kennedy Muinde <kennedymuinde@gmail.com>
2023-03-13 15:52:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+/* function multiply(num1, num2) {
+    return num1 * num2;
+}
+ */
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+
+let max = Math.max(10, 15, 13, 20);
